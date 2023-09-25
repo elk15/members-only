039599e39a7e0e14e96d14dbea51d2d52d381222
elina <papadelina145@gmail.com>
2023-09-25 19:02:14 +0300

    Add validation to sign up

--- controllers/authenticationController.js
+++ controllers/authenticationController.js
@@ -2,24 +2,66 @@ const asyncHandler = require("express-async-handler");
 var passport = require('passport');
 const User = require("../models/user");
 const bcrypt = require('bcryptjs');
+const { body, validationResult } = require("express-validator");
+
 
 exports.sign_up_get = (req, res, next) => {
     res.render("sign_up_form", {title: "Sign Up"});
 };
 
-exports.sign_up_post = asyncHandler(async (req, res, next) => {
-    const user = new User({
-        username: req.body.username,
-    });
-    bcrypt.hash(req.body.password, 10, async (err, hashedPassword) => {
-        if (err) {
-            throw err;
+exports.sign_up_post = [
+    body("username")
+        .trim()
+        .notEmpty()
+        .withMessage("Username is required")
+        .custom(async value => {
+            const user = await User.findOne({username: value}).exec();
+            if (user) {
+                throw new Error('Username already in use.');
+            }
+        }),
+    body("password")
+        .trim()
+        .notEmpty()
+        .withMessage("Password is required")
+        .isLength({min: 8})
+        .withMessage("Password must have at least 8 characters")
+        .matches(/^(?=.*[a-zA-Z])(?=.*\d)(?=.*[^a-zA-Z\d]).+$/)
+        .withMessage("Password must have at least one letter, digit and special character"),
+    body("passwordConfirm")
+        .trim()
+        .notEmpty()
+        .withMessage("Confirm password")
+        .custom((value, {req}) => {
+            return value === req.body.password;
+        })
+        .withMessage("Passwords don't match"),
+    
+    asyncHandler(async (req, res, next) => {
+        const errors = validationResult(req);
+
+        const user = new User({
+            username: req.body.username,
+        });
+
+        if (!errors.isEmpty()) {
+            res.render("sign_up_form", {title: "Sign Up", user: user, errors: errors.array()});
+        } else {
+            bcrypt.hash(req.body.password, 10, async (err, hashedPassword) => {
+                if (err) {
+                    throw err;
+                }
+                user.password = hashedPassword;
+                const result = await user.save();
+                res.redirect("/log-in");
+            });
         }
-        user.password = hashedPassword;
-        const result = await user.save();
-        res.redirect("/log-in");
-    });
-})
+            
+    })
+
+]
+
+
 
 exports.log_in_get = asyncHandler(async (req, res, next) => {
     res.render("log_in_form", {title: "Log In"});
