6fd4daae0a375c410f0385eb4975fa296aa86764
elina <papadelina145@gmail.com>
2023-09-25 12:00:05 +0300

    Create index routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const indexController = require("../controllers/indexController");
+const authenticationController = require("../controllers/authenticationController");
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', indexController.get_most_recent);
+
+router.get('/sign-up', authenticationController.sign_up_get);
+
+router.post('/sign-up', authenticationController.sign_up_post);
+
+router.get('/log-in', authenticationController.log_in_get);
+
+router.post('/log-in', authenticationController.log_in_post);
+
+router.get('/log-out', authenticationController.log_out_get);
 
 module.exports = router;
