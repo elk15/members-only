9b275b5065b26658eca51fa972214ea4da143680
elina <papadelina145@gmail.com>
2023-09-26 11:33:11 +0300

    Create user page

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,10 +4,10 @@ const Post = require("../models/post");
 
 exports.user_get_posts = asyncHandler(async (req, res, next) => {
     const [user, userPosts] = await Promise.all([
-        User.find({_id: req.params.id}, "username").exec(),
+        User.findOne({_id: req.params.id}, "username").exec(),
         Post.find({user: req.params.id}).sort({createdAt: -1}).populate("user").exec(),
     ]) 
-    res.render("user_page_posts", {title: user.username, posts: userPosts, user: user});
+    res.render("user_page", {title: user.username, posts: userPosts, user: user});
 })
 
 exports.user_delete_get = (req, res, next) => {
